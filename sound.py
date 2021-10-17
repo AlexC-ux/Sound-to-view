1c341e20bb0fcf03e116cbd5b9b96b516a6a8198
AlexC-ux <55258939+AlexC-ux@users.noreply.github.com>
2021-10-17 17:04:04 +0300

    Update sound.py

--- sound.py
+++ sound.py
@@ -1,4 +1,6 @@
 import turtle as t
+import math
+import random
 from colormap import rgb2hex
 import base64
 
@@ -9,22 +11,20 @@ for e in enc:
 filling=False
 code_arr=list(code)
 t.speed(0)
+i = 0
+t.pensize(5)
 for symb in code_arr:
+    i+=1
     t.color(rgb2hex(int(code_arr[code_arr.index(symb)-1])*20,int(symb)*20,int(code_arr[code_arr.index(symb)+1])*20))
 
-    #if (code_arr.index(symb)%300==0):
-        #t.goto(int(code_arr[code_arr.index(symb)-1])*3,int(code_arr[code_arr.index(symb)+1])*3)
-    t.goto(int(code_arr[code_arr.index(symb)-1])*3,int(code_arr[code_arr.index(symb)+1])*3)
-    t.right(code_arr.index(symb))
-    t.forward(int(symb)*20)
-    t.right(int(symb)*-11)#убрать минус у 11 потом
-    t.forward(int(symb)**2)
-    if (code_arr.index(symb)%5==0):
+    t.goto((int(symb)+1)*9,(int(symb)+1)*9)
+    t.right((int(symb)+random.randint(1, 20))*20)
+    t.forward((int(symb)+1)*27)
+    if (i%2==0):
         if (not filling):
             t.begin_fill()
             filling=True
-    if (code_arr.index(symb)%3==0):
+    if (i%3==0):
         if (filling):
             filling=False
             t.end_fill()
-        
